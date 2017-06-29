80dfe42796b8d78b07add5ac8a9cce6e33280a5f
matthewglassman <matthewglassmn78@gmail.com>
2017-06-29 16:34:12 -0400

    Added files demonstrating very basic variables holding messages and printing the variable

---
+++ Variables-Datatypes/simplemessage.py
@@ -0,0 +1,2 @@
+simple_message = "To boldly go where several have already dared to go."
+print(simple_message)
---
+++ Variables-Datatypes/simplemessages.py
@@ -0,0 +1,5 @@
+simple_message1 = "This is foo"
+print(simple_message1)
+
+simple_message1 = "This is bar"
+print(simple_message1)
