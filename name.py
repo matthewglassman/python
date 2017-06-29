aa175dd0eeab27747722e7f4b8317d478777806f
matthewglassman <matthewglassmn78@gmail.com>
2017-06-29 15:43:58 -0400

    New branch with beginner Hello World and basic files

---
+++ name.py
@@ -0,0 +1,16 @@
+name = "ada lovelace"
+print(name.title()) #Takes the string in variable name and applies the Title Case method
+
+print(name.upper()) #Takes the string in variable name and makes all letters upper case
+print(name.lower()) #Takes the string in variable name and makes all letters lower case
+
+
+first_name = "ada"
+last_name = "lovelace"
+
+full_name = first_name + " " + last_name #Concatenating strings
+print(full_name)
+print("Hello " + full_name.title() + "!")
+
+message = ("Hello " + full_name.title() + "!")
+print (message)
---
+++ whitespace.py
@@ -0,0 +1,11 @@
+print("Python")
+
+print("\tPython") # \t adds a tab
+
+print("Languages:\nPython\nC\nJavaScript") # \n creates a new line
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+print(favorite_language)
+
+print(favorite_language.rstrip()) # Removes added white space to the right of work.  lstrip() is to the left.
